ef09496ef4dde3fac788a79798f54a5a2507f591
Krissh Verma <krisshverma25@gmail.com>
2026-02-05 16:33:20 +0530

    model trained

--- ml/preprocessing/train_test_data.py
+++ ml/preprocessing/train_test_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
-from preprocess import preprocess_data
+from .preprocess import preprocess_data
 
-df=pd.read_csv("ml/data/delhi_traffic_features.csv")
-x,y,encoders=preprocess_data(df)
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
-df.drop(columns=["traffic_density_level"],inplace=True)
-print(x_train.shape)
-print(x_test.shape)
-print(y_test.shape)
-print(y_train.shape)
+def train_test_split_data():
+    df=pd.read_csv("ml/data/delhi_traffic_features.csv")
+    x,y,encoders=preprocess_data(df)
+    with open("ml/encoders.pkl","wb") as f:
+        pickle.dump(encoders,f)
+    x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
+    return x_train,x_test,y_train,y_test,encoders
+
 
---
+++ ml/train_model.py
@@ -0,0 +1,24 @@
+import pandas as pd;
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from preprocessing.train_test_data import train_test_split_data
+
+x_train,x_test,y_train,y_test,encoders=train_test_split_data()
+
+rf_model = RandomForestClassifier(
+    n_estimators=150,
+    random_state=42
+)
+
+rf_model.fit(x_train,y_train)
+y_predict=rf_model.predict(x_test)
+y_predict_prob=rf_model.predict_proba(x_test)
+decoded_output=encoders["traffic_density_level"].inverse_transform(y_predict)
+
+confidences=y_predict_prob.max(axis=1)*100
+output_df=pd.DataFrame({
+    "predicted_value":decoded_output,
+    "confidence":confidences.round(2)
+})
+
+print(output_df)
